5e11c1ab260518b1c8eb6a909563d87aced6567c
Rizwan11903 <U2367279@unimail.hud.ac.uk>
2025-09-30 14:58:25 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Introduction to PHP</title>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  </head>
+  <body>
+    <?php
+  echo "Welcome to CHT2520";
+  ?>
+  </body>
+</html>
+```html
